86270f87257ac3427f71f1d30591b35353526e74
“raedawwad95” <awwad12121@gmail.com>
2018-02-20 09:26:30 +0200

    Oop

--- Oop.js
+++ Oop.js
@@ -2,29 +2,60 @@
 // Refactor the code as an MakeGame class that shares its methods across different instances.
 
 
-function randInt(n) {
-    return Math.floor(Math.random() * (n + 1));
+// function randInt(n) {
+//     return Math.floor(Math.random() * (n + 1));
+// }
+
+// function makeGame(upperbound){
+//   var upper=upperbound;
+//   var rand=randInt(upper)
+//   var counter=0;
+//   return {
+//   	guessMyNumber:function(n){
+//   		counter++;
+// 	    if (n > upper) {
+// 	      return "Out of bounds! Please try a number between 0 and " + upperbound + ".";
+// 	    } else if (n === rand) {
+// 	      return "You guessed my number!";
+// 	    }
+// 	    return "Nope! That wasn't it!";
+//   	},
+//   	giveUp:function(){
+//     	return rand;
+//   	},
+//   	numOfGuesses:function(){
+//     	return counter;
+//   	}
+//   }
+// }
+
+function makeGameclass(upperbound){
+  var instances ={};
+  instances.upper = upperbound;
+  instances.rand=randInt(upperbound);
+  instances.counter=0;
+  instances.guessMyNumber=guessMyNumber;
+  instances.giveUp=giveUp;
+  instances.numOfGuesses=numOfGuesses;
+  return instances;
 }
 
-function makeGame(upperbound){
-  var upper=upperbound;
-  var rand=randInt(upper)
-  var counter=0;
-  return {
-  	guessMyNumber:function(n){
-  		counter++;
-	    if (n > upper) {
-	      return "Out of bounds! Please try a number between 0 and " + upperbound + ".";
-	    } else if (n === rand) {
-	      return "You guessed my number!";
-	    }
-	    return "Nope! That wasn't it!";
-  	},
-  	giveUp:function(){
-    	return rand;
-  	},
-  	numOfGuesses:function(){
-    	return counter;
-  	}
-  }
+function randInt(n) {
+     return Math.floor(Math.random() * (n + 1));
+ }
+var guessMyNumber = function(n){
+  this.counter++;
+  if(n>this.upper){
+    return "Out of bounds! Please try a number between 0 and " + this.upperbound + ".";
+      } else if (n === this.rand) {
+        return "You guessed my number!";
+      }
+    return "Nope! That wasn't it!";
+}
+
+var giveUp = function(){
+  return this.rand;
+}
+var numOfGuesses = function(){
+  return this.counter;
 }

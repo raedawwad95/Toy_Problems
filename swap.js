fac17d3ed3bfdf60d55220a94ff8f4f808025efa
“raedawwad95” <awwad12121@gmail.com>
2018-02-28 09:20:53 +0200

    swap

--- swap.js
+++ swap.js
@@ -3,4 +3,14 @@
 	// ToYPRoblEm --> tOyprOBLeM
 	function swap(input){
 		//your code is here
+		var str ='';
+		for (var i = 0; i < input.length; i++) {
+			if(input[i] ===input[i].toUpperCase()){
+
+				str +=input[i].toLowerCase();
+			}else{
+				str +=input[i].toUpperCase();
+			}
+		}
+		return str;
 	}

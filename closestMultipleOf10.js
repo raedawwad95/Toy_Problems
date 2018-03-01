ceb8ff12d15fe943df9f2938ec56fa3cdab140e3
“raedawwad95” <awwad12121@gmail.com>
2018-03-01 09:53:41 +0200

    closest

--- closestMultipleOf10.js
+++ closestMultipleOf10.js
@@ -5,4 +5,20 @@
 	// 37 ===> 40
  function closestMultipleOf10(num) {
  	//your code is here
+ 	if(num/10 > num%10){
+ 		if(num%10 !==0){
+ 			return closestMultipleOf10(num-1);
+ 		}else{
+ 			return num;
+ 		}
+
+ 	}else{
+ 		if(num%10 !==0){
+ 			return closestMultipleOf10(num+1);
+ 	}else{
+ 		return num
+ 	}
+ 	}
+ 	
+
  }
